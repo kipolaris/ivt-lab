64236485d774afcd491e67a72dd6df555d983e72
kipo <csutoralilla25@gmail.com>
2024-04-19 10:51:00 +0200

    Implemented

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,7 +77,7 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedo stores
+        // try to fire both of the torpedo stores: implemented
         firingSuccess = primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1);
         break;
     }
